0226c1408fa111fcc2e542a5fa81d71f85d1c05b
Frederico Rodrigues <frederico2191@gmail.com>
2023-02-10 12:22:07 +0000

    included 18BAR

--- src/app.js
+++ src/app.js
@@ -6,57 +6,42 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  // confirm("Ready to play?");
-  // var age = prompt("How old are you?");
-  // Get a reference to the element we want to update
-  //   el = document.getElementById("result"),
-  //   message;
+  confirm("Ready to play?");
+  const age = prompt("How old are you?");
+  if (age >= 18) {
+    const suitHolders = document.querySelectorAll(".suit");
+    // console.log("suitHolders", suitHolders);
+    const cardHolder = document.getElementById("numberSelected");
+    // console.log("cardHolder", cardHolder);
+    const suitRamdomizer = Math.floor(Math.random() * 3);
+    // console.log("suitRamdomizer", suitRamdomizer);
+    const cardRamdomizer = Math.floor(Math.random() * 13);
+    // console.log("cardRamdomizer", cardRamdomizer);
 
-  // // Check the age and set the message variable based on that
-  // if (age >= 18) {
-  //   message = "Let's get started then!";
-  // } else {
-  //   message = "You're under 18? Be careful out there....";
-  // }
-  // // Update the content of the element with the message
-  // el.innerHTML = message;
+    const possibleSuitsArray = ["♥", "♦", "♣", "♠"];
+    const possibleNumbersArray = [
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      "J",
+      "D",
+      "K",
+      "A",
+    ];
 
-  const suitHolders = document.querySelectorAll(".suit");
-  console.log("suitHolders", suitHolders);
+    document
+      .querySelectorAll(".suit")
+      .forEach((el) => (el.innerHTML = possibleSuitsArray[suitRamdomizer]));
 
-  const cardHolder = document.getElementById("numberSelected");
-  console.log("cardHolder", cardHolder);
-
-  const suitRamdomizer = Math.floor(Math.random() * 3);
-  console.log("suitRamdomizer", suitRamdomizer);
-
-  const cardRamdomizer = Math.floor(Math.random() * 13);
-  console.log("cardRamdomizer", cardRamdomizer);
-
-  const possibleSuitsArray = ["♥", "♦", "♣", "♠"];
-  const possibleNumbersArray = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "D", "K", "A"];
-
-  document
-    .querySelectorAll(".suit")
-    .forEach(el => (el.innerHTML = possibleSuitsArray[suitRamdomizer]));
-
-  document.getElementById("numberSelected").innerHTML =
-    possibleNumbersArray[cardRamdomizer];
+    document.getElementById("numberSelected").innerHTML =
+      possibleNumbersArray[cardRamdomizer];
+  } else {
+    document.write("You're under 18? Be careful out there....");
+  }
 };
-
-// function ayee() {
-//   confirm("Ready to play?");
-//   var age = prompt("How old are you?"),
-//     // Get a reference to the element we want to update
-//     el = document.getElementById("result"),
-//     message;
-
-//   // Check the age and set the message variable based on that
-//   if (age >= 18) {
-//     message = "Let's get started then!";
-//   } else {
-//     message = "You're under 18? Be careful out there....";
-//   }
-//   // Update the content of the element with the message
-//   el.innerHTML = message;
-// }
