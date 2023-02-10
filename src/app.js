1f7413fa68d2d3441f599bad570dc4e106ccd25b
Frederico Rodrigues <frederico2191@gmail.com>
2023-02-10 18:41:00 +0000

    secondary menu

--- src/app.js
+++ src/app.js
@@ -2,62 +2,74 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let counter = 0;
+const counterButton = document.getElementById("counter");
+const suitHolders = document.querySelectorAll(".suit");
+const cardHolder = document.getElementById("numberSelected");
+const possibleSuitsArray = ["♥", "♦", "♣", "♠"];
+const possibleNumbersArray = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "D", "K", "A"];
+const winnerSuit = possibleSuitsArray[Math.floor(Math.random() * 3)];
+const winnerArray = possibleNumbersArray[Math.floor(Math.random() * 13)];
+const winnerCard = [winnerSuit, winnerArray];
+let playerNamesLocalStorage = [];
+let playerAge = [];
+let amountOfPlayers = 0;
 
-let cardCounter = 0;
-const counterButton = document.getElementById("cardCounter");
+function equalsCheck(a, b) {
+  if (a.length != b.length) {
+    return false;
+  } else {
+    let result = false;
+
+    // comparing each element of array
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+        return false;
+      } else {
+        result = true;
+      }
+    }
+    return result;
+  }
+}
 
 function mainDomFunction() {
-  cardCounter++;
-  if (cardCounter === 1) {
-    counterButton.textContent = ` ${cardCounter} single time played`;
+  counter++;
+  if (counter === 1) {
+    counterButton.textContent = "1 single time played so far";
   } else {
-    counterButton.textContent = ` ${cardCounter} times played`;
+    counterButton.textContent = ` ${counter} times played`;
   }
   const anewSuit = Math.floor(Math.random() * 3);
   const anewRamdomizer = Math.floor(Math.random() * 13);
-
   let checker = [
     possibleSuitsArray[anewSuit],
     possibleNumbersArray[anewRamdomizer]
   ];
 
-  console.log("checker", checker);
-  console.log("winnerCard", winnerCard);
+  const playingButtons = document.getElementById("playingButtons");
 
-  function equalsCheck(a, b) {
-    if (a.length != b.length) {
-      return false;
-    } else {
-      let result = false;
-
-      // comparing each element of array
-      for (let i = 0; i < a.length; i++) {
-        if (a[i] !== b[i]) {
-          return false;
-        } else {
-          result = true;
-        }
-      }
-      return result;
-    }
-  }
+  const afterWinMenu = document.getElementById("afterWinMenu");
+  console.log("afterWinMenu", afterWinMenu);
 
   const result = equalsCheck(checker, winnerCard);
   if (result) {
     console.log("WINNER!");
+    playingButtons.style.display = "none";
+    afterWinMenu.style.display = "flex";
     document.querySelectorAll(".suit").forEach(el => {
       el.innerHTML = '<i class="fa-duotone fa-trophy-star"></i>';
     });
-    document.getElementById("numberSelected").innerHTML = "WINNER!";
+    document.getElementById("numberSelected").innerHTML =
+      "WINNER!" + '<i class="fa-duotone fa-trophy"></i>';
     setTimeout(function() {
       alert(
-        `Congratulations! You needed only ${cardCounter} attempts to win !!!`
+        `Congratulations! ${playerNamesLocalStorage[0]}, you needed only ${counter} attempts to win !!!`
       );
     }, 100);
   } else {
-    console.log("I have to conitnue trying");
+    // afterWinMenu.style.display = "none";
+    console.log("I have to continue trying");
     document.querySelectorAll(".suit").forEach(el => {
       el.innerHTML = possibleSuitsArray[anewSuit];
     });
@@ -66,44 +78,23 @@ function mainDomFunction() {
   }
 }
 
-const suitHolders = document.querySelectorAll(".suit");
-const cardHolder = document.getElementById("numberSelected");
-
-const possibleSuitsArray = ["♥", "♦", "♣", "♠"];
-const possibleNumbersArray = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "D", "K", "A"];
-const winnerSuit = possibleSuitsArray[Math.floor(Math.random() * 3)];
-const winnerArray = possibleNumbersArray[Math.floor(Math.random() * 13)];
-const winnerCard = [winnerSuit, winnerArray];
-// console.log(winnerSuit)
-// console.log(winnerArray)
-// console.log(winnerCard)
-let playerNamesLocalStorage = [];
-let playerAge = [];
-let amountOfPlayers = 0;
-
 const anewCard = document.getElementById("anewCard");
 anewCard.onclick = mainDomFunction;
 
 window.onload = function() {
   confirm("Ready to play?");
 
-  playerAge[0] = prompt("How old are you?");
+  playerAge[amountOfPlayers] = prompt("How old are you?");
   console.log("playerAge", playerAge);
-  // localStorage.setItem("playerAge", JSON.stringify(playerAge);
-  // let playerAgeLocalStorage = JSON.parse(localStorage.getItem("playerAge")
-  // );
+  playerNamesLocalStorage[amountOfPlayers] = prompt("Provide player name :)");
 
-  // playerNamesLocalStorage[amountOfPlayers] = prompt("Provide player name :)");
-  // localStorage.setItem(
-  //   "playerNamesLocalStorage",
-  //   JSON.stringify(playerNamesLocalStorage)
-  // );
+  localStorage.setItem(
+    "playerNames",
+    JSON.stringify(playerNamesLocalStorage[amountOfPlayers])
+  );
+  localStorage.setItem("playerAge", JSON.stringify(playerAge));
 
-  // let playerNamesLocalStorage = JSON.parse(
-  //   localStorage.getItem("playerNamesLocalStorage")
-  // );
-
-  if (playerAge[0] >= 18) {
+  if (playerAge[amountOfPlayers] >= 18) {
     mainDomFunction();
   } else {
     document.write("You're under 18? Be careful out there....");
@@ -112,5 +103,34 @@ window.onload = function() {
 
 const button = document.getElementById("btn");
 button.onclick = function(event) {
-  button.textContent = `This is the ${event.detail} time you press this unreliable button!`;
+  button.textContent = `Hope you're releasing your stress! You've pressed ${event.detail} times this amnesiac button`;
+};
+
+const top = document.getElementById("top");
+top.onclick = function() {
+  console.log("topppppppp", top);
+  top.textContent = "I will be rendering the localStorage data";
+};
+const samePlayerButton = document.getElementById("samePlayerButton");
+samePlayerButton.onclick = function() {
+  console.log("samePlayerButton", samePlayerButton);
+  samePlayerButton.textContent = "I will be rendering the localStorage data";
+  amountOfPlayers++;
+  playerNamesLocalStorage[amountOfPlayers] =
+    playerNamesLocalStorage[amountOfPlayers - 1];
+  playerAge[amountOfPlayers] = playerAge[amountOfPlayers - 1];
+};
+const differentPlayerButton = document.getElementById("differentPlayerButton");
+differentPlayerButton.onclick = function() {
+  console.log("differentPlayerButton", differentPlayerButton);
+  differentPlayerButton.textContent =
+    "I will be rendering the localStorage data";
+};
+
+const exit = document.getElementById("exit");
+exit.onclick = function() {
+  console.log("exiiiiit", exit);
+  if (confirm("Alright champ! Sure you wanna go?")) {
+    window.close();
+  }
 };
